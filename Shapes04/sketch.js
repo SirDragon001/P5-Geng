9b4d93b978c24d308bd8c2d56c90343795d78da2
SirDragon001 <41339504+SirDragon001@users.noreply.github.com>
2020-10-02 16:07:56 +0200

    friday

---
+++ Shapes04/sketch.js
@@ -0,0 +1,28 @@
+
+var x = 0;
+var y = 0;
+var framesElapsed = 0;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  noStroke();
+
+  x = windowWidth / 2;
+  y = windowHeight / 2;
+}
+
+function draw() {
+  background(0);
+  
+
+  noFill();
+
+  text('Value of X: ' + x,200,80);
+  text('Value of XL: ' + xl,200,100);
+
+  
+
+
+
+}
+
